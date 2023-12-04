cdae1d93ee5456020a78417be8eb0d899cebdafd
Jeffrey Mostroso <1844253+jeffrey-omega@users.noreply.github.com>
2023-12-04 02:41:10 +0100

    Add new files & update existing files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../styles/global.scss';
+	import Navbar from '@components/ui/Navbar.svelte';
+	import { initializeStores, AppShell } from '@skeletonlabs/skeleton';
+
+	initializeStores();
+
+	let title: string = 'Home';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<Navbar />
+	</svelte:fragment>
+	<slot />
+</AppShell>
