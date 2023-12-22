19c03771e7cf3bc420b3152ddd4a67a97ac153e6
Jeffrey Mostroso <1376038-jeffrey-omega@users.noreply.gitlab.com>
2023-12-22 20:12:37 +0100

    Add dark mode and light mode tests for displaying video

--- tests/test.ts
+++ tests/test.ts
@@ -1,17 +1,35 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('index page', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/');
-	});
 
-	test('display video', async ({ page }) => {
-		await page.waitForLoadState('domcontentloaded');
+    test.describe('dark mode', () => {
+        test.use({ colorScheme: 'dark' });
 
-		const video = await page.$('video');
-		expect(video).toBeTruthy();
+        test.beforeEach(async ({ page }) => {
+            await page.goto('/');
+        });
 
-		// generate a screenshot of the page
-		await page.screenshot({ path: 'static/screenshot.png', fullPage: true });
-	});
+        test('display video with dark mode', async ({ page }) => {
+            await page.waitForLoadState('domcontentloaded');
+            const video = await page.$('video');
+            expect(video).toBeTruthy();
+            await page.screenshot({ path: 'static/screenshot-dark.png', fullPage: true });
+        });
+    });
+
+    test.describe('light mode', () => {
+        test.use({ colorScheme: 'light' });
+
+        test.beforeEach(async ({ page }) => {
+            await page.goto('/');
+        });
+
+        test('display video with light mode', async ({ page }) => {
+            await page.waitForLoadState('domcontentloaded');
+            const video = await page.$('video');
+            expect(video).toBeTruthy();
+            await page.screenshot({ path: 'static/screenshot-light.png', fullPage: true });
+        });
+    });
+
 });
